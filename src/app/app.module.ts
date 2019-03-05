1867003b1a577a47c3ad232f37f19a7d35c11b03
Mathieu Steinmetz <mathieu.steinmetz22@gmail.com>
2019-03-06 00:09:18 +0100

    Adding socket.io module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { ScreenViewComponent } from './screen-view/screen-view.component';
 
+const config: SocketIoConfig = { url: 'http://localhost:4444', options: {} };
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScreenViewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SocketIoModule.forRoot(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
