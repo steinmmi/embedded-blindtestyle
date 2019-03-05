d3085aa4d98b45b4a1329974411146c53d9dad71
Mathieu Steinmetz <mathieu.steinmetz22@gmail.com>
2019-03-06 00:14:56 +0100

    Adding PlayerViewComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ScreenViewComponent } from './screen-view/screen-view.component';
+import { PlayerViewComponent } from './player-view/player-view.component';
 
 const routes: Routes = [
-  {path: '', component: ScreenViewComponent}
+  {path: '', component: ScreenViewComponent},
+  {path: 'play', component: PlayerViewComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 import { ScreenViewComponent } from './screen-view/screen-view.component';
+import { PlayerViewComponent } from './player-view/player-view.component';
 
 const config: SocketIoConfig = { url: 'http://localhost:4444', options: {} };
 @NgModule({
   declarations: [
     AppComponent,
-    ScreenViewComponent
+    ScreenViewComponent,
+    PlayerViewComponent
   ],
   imports: [
     BrowserModule,
