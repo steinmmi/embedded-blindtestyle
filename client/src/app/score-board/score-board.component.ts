726f2030c629ac74dd55ba599302f7610ee8eda6
Mathieu Steinmetz <mathieu.steinmetz22@gmail.com>
2019-03-06 23:06:10 +0100

    Improve ScoreBoard

--- client/src/app/score-board/score-board.component.ts
+++ client/src/app/score-board/score-board.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PlayersService } from '../players.service';
 
 @Component({
   selector: 'app-score-board',
@@ -7,36 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScoreBoardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private playersService: PlayersService) { }
   players: Array<Object>;
-
-  getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
 
   ngOnInit() {
 
-    // TODO get players from server
-    this.players = [
-      {name: "Mathieu", score: 15},
-      {name: "Margaux", score: 12},
-      {name: "Léo", score: 98},
-      {name: "Mathilde", score: 21},
-      {name: "Adrien", score: 11},
-      {name: "JohnnyYesPapa", score: 25},
-    ]
-    this.players.sort(function(a, b){
-      return b['score']-a['score']
-    })
-    let colors = ['red','blue','green','purple','gold','orange']
-    this.players.map((el) => {
-      el['color'] = colors.shift()
-    })
+    this.players = this.playersService.getPlayers();
   }
 
 }
