961574297ab906ee6c784d1e23d7e678436101ab
root <root@vps645673.ovh.net>
2019-03-06 10:35:57 +0100

    Adding PhoneGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ScreenViewComponent } from './screen-view/screen-view.component';
 import { PlayerViewComponent } from './player-view/player-view.component';
+import { PhoneGuard } from './phone.guard';
 
 const routes: Routes = [
   {path: '', component: ScreenViewComponent},
-  {path: 'play', component: PlayerViewComponent},
+  {path: 'play', component: PlayerViewComponent,canActivate: [PhoneGuard]},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 import { ScreenViewComponent } from './screen-view/screen-view.component';
 import { PlayerViewComponent } from './player-view/player-view.component';
+import { PhoneGuard } from './phone.guard';
 
 const config: SocketIoConfig = { url: 'http://localhost:4444', options: {} };
 @NgModule({
@@ -19,7 +20,7 @@ const config: SocketIoConfig = { url: 'http://localhost:4444', options: {} };
     AppRoutingModule,
     SocketIoModule.forRoot(config)
   ],
-  providers: [],
+  providers: [PhoneGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
