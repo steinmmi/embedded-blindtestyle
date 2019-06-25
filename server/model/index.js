0b8075a40c9943774b990658e8d760177fa58932
Mathieu Steinmetz <m.steinmetz@cyim.com>
2019-06-25 14:16:30 +0200

    Refactor DB connection

--- server/model/index.js
+++ server/model/index.js
@@ -1,45 +1,48 @@
 const mongodb = require('mongodb')
 const ObjectID = mongodb.ObjectID;
 const MongoClient = mongodb.MongoClient;
-const log = require('../modules/log')
+const log = require('../modules/log');
+
+
+var db;
+// Connect to database
+MongoClient.connect("mongodb://localhost/", { useNewUrlParser: true },function(error, client) {
+    if (error) throw error;
+    db = client.db('blindtestyle');
+});
+
+
 
 let model = {
     songs: {
         getRandom: () => {
             return new Promise((resolve, reject) => {
-            MongoClient.connect("mongodb://localhost/", { useNewUrlParser: true },function(error, client) {
-                if (error) throw error;
-                db = client.db("blindtestyle");
                 db.collection("songs").aggregate([ { $sample: { size: 1 } } ]).toArray((err, doc) => {
-                    resolve(doc[0])
-                });
-            })
-        })
-        },
-        get: (id) => {
-            MongoClient.connect("mongodb://localhost/", { useNewUrlParser: true },function(error, client) {
-                if (error) throw error;
-                db = client.db("blindtestyle");
-                db.collection("songs").findOne({'_id': new ObjectID(id)}, (err, doc) => {
-                    return true;
+                    if(err) reject(err);
+                    resolve(doc[0]);
                 });
             });
-            return 0;
+        },
+        get: (id) => {
+            return new Promise((resolve, reject) => {
+                db.collection("songs").findOne({'_id': new ObjectID(id)}, (err, doc) => {
+                    if(err) reject(err);
+                    resolve(doc);
+                });
+            });
         },
         insertOne: (song, file) => {
-            MongoClient.connect("mongodb://localhost/", { useNewUrlParser: true }, function(error, client) {
-            if (error) throw error;
-            db = client.db("blindtestyle");
+            return new Promise((resolve, reject) => {
             db.collection("songs").insertOne(song, null, function(error, results) {
                 if (error) throw error;
                 let sampleFile = file;
                 sampleFile.mv(`./songs/${results.insertedId}.mp3`, (err) => {
-                if(err) throw err
+                    if(err) reject(err);
+                    log.info('New file uploaded');
+                    resolve();
+                });
             });
-            log.info('File uploaded')
-            return true;
         });
-    });
         }
     }
 }
