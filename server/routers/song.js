ea37399f5ae8326e9ae662baf4f945500c9fa19b
Mathieu Steinmetz <m.steinmetz@cyim.com>
2019-06-25 14:53:21 +0200

    Use primise for Model

--- server/routers/song.js
+++ server/routers/song.js
@@ -1,33 +1,34 @@
 
 const express = require('express')
 const router = express.Router();
-const ObjectID = require('mongodb').ObjectID;
-const MongoClient = require("mongodb").MongoClient;
 const fs = require('fs');
 const Model = require('../model');
 
 router.post("/add", function(req, res) {
-    res.send(Model.songs.insertOne(req.body, req.files.file))
+    Model.songs.insertOne(req.body, req.files.file).then(() => {
+        res.send(true);
+    }).catch(err => {
+        res.send(err);
+    })
 });
 
 router.get('/get/:id', (req, res) => {
     let path = `./songs/${req.params.id}.mp3`;
     try {
         if (fs.existsSync(path)) {
-          res.download(path)
+            res.download(path);
         }
         else throw new Error(`No such file : ${req.params.id}.mp3`)
-      } catch(err) {
+    } catch(err) {
         res.send(err.message)
-      }
+    }
 });
 router.get('/info/:id', (req, res) => {
-    let id = req.params.id
-    res.send(Model.songs.get(id))
-});
-
-router.get('/get', (req, res) => {
-    res.send('oui')
-    Model.songs.getRandom();
+    let id = req.params.id;
+    Model.songs.get(id).then(val => {
+        res.send(JSON.stringify(val));
+    }).catch(err => {
+        res.send(err);
+    });
 });
 module.exports = router;
